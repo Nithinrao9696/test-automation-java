ce6341168c6b652396ad6755845f29d4c43c5e65
Nithinrao9696 <30919697+Nithinrao9696@users.noreply.github.com>
2020-02-20 20:11:42 -0500

    Update HomePageTests.java

--- src/test/java/tests/HomePageTests.java
+++ src/test/java/tests/HomePageTests.java
@@ -1,37 +1,53 @@
-package basicsOfSelenium;
 
-import java.net.MalformedURLException;
+import static junit.framework.Assert.assertEquals;
 import java.net.URL;
-
+import org.apache.log4j.Logger;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.annotations.Test;
-
-public class ArrayDemo {
-
-    @Test
-
-    public void mailTest() throws MalformedURLException{
-
-             DesiredCapabilities dr=null;
-
-             dr=DesiredCapabilities.firefox();
-
-             dr.setBrowserName("firefox");
-
-             dr.setPlatform(Platform.LINUX);
-
-             RemoteWebDriver driver=new RemoteWebDriver(new URL("http://ap-jenkdx-dev:4444/wd/hub"), dr);
-
-             driver.navigate().to("https://www.google.co.in");
-
-             driver.findElement(By.xpath("//*[@id='gs_htif0']")) .sendKeys("Search me");
-
-             driver.findElement(By.xpath("//*[@id='tsf']/div[2]/div[3]/center/input[1]")) .click();
-
-             driver.close();
-
-}
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+/**
+ * This class is responsible to test the UserInterface using Selenium Grid concept
+ */
+public class UISeleniumGridTest {
+  private static final Logger log = Logger.getLogger(UISeleniumGrid.class);
+  private static Wait<WebDriver> wait;
+  private static DesiredCapabilities capabillities;
+  private static WebDriver driver;
+  @BeforeClass
+  public static void setUp() throws Exception {  
+    capabillities = DesiredCapabilities.firefox();
+    /** URL is the selenium hub URL here **/
+    driver = new RemoteWebDriver(new URL("http://ap-jenkdx-dev:4444/wd/hub"), capabillities);
+    capabillities.setBrowserName("firefox"); 
+    wait = new WebDriverWait(driver, 6000);
+  }
+  /**
+     * To test the UI
+     * @throws Exception
+     */
+  @Test
+  public void testUI() throws Exception {     
+    /** Your application URL which you want to test **/
+    driver.get("http://localhost:8090/myapp/index.html"); 
+    wait.until(new ExpectedCondition<Boolean>() {  
+      public Boolean apply(WebDriver webDriver) {     
+        log.info("Please be patience .... Searching ...");  
+        return webDriver.findElement(By.tagName("title")) != null;          
+      }         
+    });
+    log.info(driver.findElement(By.tagName("body")).getText());
+    assertEquals("URL must be http://localhost:8090/myapp/index.html", "http://localhost:8090/myapp/index.html", driver.getCurrentUrl());
+    /** put other asserts as well **/
+  }
+  @AfterClass
+  public static void tearDown() throws Exception {
+    driver.quit();  
+  }
 }

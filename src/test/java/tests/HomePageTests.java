0f018cb5ee3956195f44d4364542a7e8b6c1902e
Nithinrao9696 <30919697+Nithinrao9696@users.noreply.github.com>
2020-02-20 20:17:07 -0500

    Update HomePageTests.java

--- src/test/java/tests/HomePageTests.java
+++ src/test/java/tests/HomePageTests.java
@@ -16,7 +16,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
  * This class is responsible to test the UserInterface using Selenium Grid concept
  */
 public class HomePageTests {
-  private static final Logger log = Logger.getLogger(UISeleniumGrid.class);
+  private static final Logger log = Logger.getLogger(HomePage.class);
   private static Wait<WebDriver> wait;
   private static DesiredCapabilities capabillities;
   private static WebDriver driver;
